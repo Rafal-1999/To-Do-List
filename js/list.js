ca9bd62b80f0f4b29a498f4280a0eb2d45acffc2
Rafal-1999 <skapinka1999@wp.pl>
2021-12-09 22:39:14 +0100

    Created function init with variables

---
+++ js/list.js
@@ -0,0 +1,9 @@
+{
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        const newTask = document.querySelector(".js-newTask");
+        const taskBlock = document.querySelector(".js-taskBlock");
+    }
+
+    init();
+}
