b33cd5130bb08fff329ef073372d4bdd3800a050
Rafal-1999 <skapinka1999@wp.pl>
2022-04-01 22:19:19 +0200

    Changed toggleDoneTask according to immutability

--- js/list.js
+++ js/list.js
@@ -59,7 +59,15 @@
     };
 
     const toggleDoneTask = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done
+            },
+            ...tasks.slice(taskIndex + 1)
+        ];
+
         render();
     };
 
